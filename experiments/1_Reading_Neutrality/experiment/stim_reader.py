11aa89b26593e4fd56c0bed05791bd4f44bc109a
BranPap <brandonpapineau254@gmail.com>
2021-04-07 13:03:19 -0700

    added .py file for csv to JSON conversion

---
+++ experiments/1_Reading_Neutrality/experiment/stim_reader.py
@@ -0,0 +1,32 @@
+import json
+import pandas as pd
+
+df = pd.read_csv('stims.csv')
+
+headers = ["name","be","det","target","prep","state","pro","like","activity"]
+
+entries = []
+
+for index,row in df.iterrows():
+    sentence = dict()
+    words = []
+    for term in headers:
+        d = dict()
+        d["form"] = row[term]
+        words.append(d)
+    sentence["words"] = words
+    sentence["question1"] = row["question1"]
+    sentence["answer1"] = row["answer1"]
+    sentence["question2"] = row["question2"]
+    sentence["answer2"] = row["answer2"]
+    entries.append(sentence)
+
+with open('stims.json', 'w') as stimlist:
+    s = json.dumps(entries, indent=4)
+    stimlist.write(s)
+
+
+# with open('stims.csv', 'r') as stimlist:
+#     for line in stimlist:
+#         line_split = line.split(',')
+#         print line_split
